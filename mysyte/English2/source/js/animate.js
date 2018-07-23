00fb70c913b6591dd2c93c6564003cc725e36691
Артём <tema1411@yahoo.com>
2018-07-23 15:58:06 +0300

    Отключение анимации на телефоне и планшете

--- mysyte/English2/source/js/animate.js
+++ mysyte/English2/source/js/animate.js
@@ -3,14 +3,15 @@
 //Example: $(".element").animated("zoomInUp");
 //Author URL: http://webdesign-master.ru
 $(document).ready(function () {
-    (function($) {
-        $.fn.animated = function(inEffect) {
-            $(this).each(function() {
+    (function ($) {
+        $.fn.animated = function (inEffect) {
+            $(this).each(function () {
                 var ths = $(this);
-                ths.css("opacity", "0").addClass("animated").waypoint(function(dir) {
+                ths.css("opacity", "0").addClass("animated").waypoint(function (dir) {
                     if (dir === "down") {
                         ths.addClass(inEffect).css("opacity", "1");
-                    };
+                    }
+                    ;
                 }, {
                     offset: "90%"
                 });
@@ -19,16 +20,25 @@ $(document).ready(function () {
         };
     })(jQuery);
 
-    $('button:not(:first-of-type)').animated('zoomInUp');
+    var animatesFunction = function () {
+        $('button:not(:first-of-type)').animated('zoomInUp');
+        $('.advantages__item, .dispatch__container, .advantages__buttons, .about__video').animated('rotateInDownRight');
+        $('.main-caption, .h2').animated('flip');
+        $('.form-item').animated('zoomIn');
+        $('.step, .step__button-container, footer, .about .services, .advantages, .dispatch').animated("slideInUp");
+        $('.step__item--1, .step__item--4,.step__item--7, .step__item--9, .step__item--8, .about__text,.services__price').animated("fadeInRight");
+        $('.step__item--2, .step__item--5, .step__item--3, .step__item--6, .step__description, .service__program, .about__video').animated("fadeInLeft");
+    };
+    var windowWidth = window.innerWidth;
+    if (windowWidth >= 1040) {
+        animatesFunction()
+    }
 
-    $('.advantages__item, .dispatch__container, .advantages__buttons, .about__video').animated('rotateInDownRight');
+    window.onresize = function (ev) {
+        windowWidth = window.innerWidth;
+        if (windowWidth >= 1040) {
+            animatesFunction()
+        }
+    }
 
-    $('.main-caption, .h2').animated('flip');
-
-    $('.form-item').animated('zoomIn');
-
-
- $('.step, .step__button-container, footer, .about .services, .advantages, .dispatch' ).animated("slideInUp");
- $('.step__item--1, .step__item--4,.step__item--7, .step__item--9, .step__item--8, .about__text,.services__price').animated("fadeInRight");
- $('.step__item--2, .step__item--5, .step__item--3, .step__item--6, .step__description, .service__program, .about__video').animated("fadeInLeft");
 });
