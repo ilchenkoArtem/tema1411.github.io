1286101fcd74f013830d5d7e130d556bf413a24a
Артём <tema1411@yahoo.com>
2018-07-23 17:21:02 +0300

    Правки по анимациям

--- mysyte/English2/source/js/animate.js
+++ mysyte/English2/source/js/animate.js
@@ -1,7 +1,7 @@
 // Управление анимацией
 //Animate CSS + WayPoints javaScript Plugin
 //Example: $(".element").animated("zoomInUp");
-//Author URL: http://webdesign-master.ru
+//Author URL: http://webdesign-mas
 $(document).ready(function () {
     (function ($) {
         $.fn.animated = function (inEffect) {
@@ -22,12 +22,11 @@ $(document).ready(function () {
 
     var animatesFunction = function () {
         $('button:not(:first-of-type)').animated('zoomInUp');
-        $('.advantages__item, .dispatch__container, .advantages__buttons, .about__video').animated('rotateInDownRight');
-        $('.main-caption, .h2').animated('flip');
+        $('.advantages__item, .dispatch__container').animated('rotateInDownRight');
         $('.form-item').animated('zoomIn');
-        $('.step, .step__button-container, footer, .about .services, .advantages, .dispatch').animated("slideInUp");
-        $('.step__item--1, .step__item--4,.step__item--7, .step__item--9, .step__item--8, .about__text,.services__price').animated("fadeInRight");
-        $('.step__item--2, .step__item--5, .step__item--3, .step__item--6, .step__description, .service__program, .about__video').animated("fadeInLeft");
+        $('.step, .step__button-container, footer, .about .services, .advantages, .dispatch, .about__text, .about__video, .main-caption, .advantages__buttons, .h2 ').animated("slideInUp");
+        $('.step__item--1, .step__item--4,.step__item--7, .step__item--9, .step__item--8,.services__price').animated("fadeInRight");
+        $('.step__item--2, .step__item--5, .step__item--3, .step__item--6, .step__description, .service__program').animated("fadeInLeft");
     };
     var windowWidth = window.innerWidth;
     if (windowWidth >= 1040) {
