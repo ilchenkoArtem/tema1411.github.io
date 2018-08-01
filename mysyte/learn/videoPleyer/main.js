4724b9e837444662a3cb05af1fec27536e1861d6
Артём <tema1411@yahoo.com>
2018-08-01 21:02:54 +0300

    Перенос файлов и кастомный плеер

---
+++ mysyte/learn/videoPleyer/main.js
@@ -0,0 +1,27 @@
+var videoElement = document.querySelector('video');
+var playButton = document.querySelector('.playVidio');
+var containerVideo = document.querySelector('.container__video');
+
+var noPlayVideo = function () {
+    videoElement.pause();
+    playButton.classList.remove('playVidio--opacity');
+}
+
+var resizeVideo = function () {
+    containerVideo.classList.toggle('bigSize');
+}
+
+var onPlayVideo = function () {
+    playButton.classList.add('playVidio--opacity');
+    videoElement.play();
+    playButton.removeEventListener('click', onPlayVideo)
+    videoElement.removeEventListener('click', onPlayVideo);
+    videoElement.addEventListener('mouseover', onPlayVideo);
+    videoElement.addEventListener('mouseleave', noPlayVideo);
+    videoElement.addEventListener('click', resizeVideo);
+}
+
+
+
+videoElement.addEventListener('click', onPlayVideo);
+playButton.addEventListener('click', onPlayVideo)
